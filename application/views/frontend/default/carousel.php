1f145fca5dfe513adaf525d919711852d8f37f8e
Kira Tran <cnttck@gmail.com>
2020-04-10 01:02:35 +0700

    load

--- application/views/frontend/default/carousel.php
+++ application/views/frontend/default/carousel.php
@@ -1,10 +1,5 @@
 <?php defined('SYSPATH') or die('No direct script access.'); ?>
 
-<!--0.0.0.0 account.jetbrains.com-->
-<!--1.2.3.4 account.jetbrains.com-->
-<!--1.2.3.4 http://www.jetbrains.com-->
-<!--1.2.3.4 www-weighted.jetbrains.com-->
-
 <!-- BEGIN: carousel -->
 <c:if is="$route_name == 'home'">
 <!--   <div class="block-title-top">-->
@@ -21,207 +16,57 @@
             <div class="carousel-inner no-padding row">
 
                 <div class="carousel-item active">
-                    <div class="f_item">
-                        <a href="http://google.com" title="Để mai tính">
-                            <div class="f_filmthumb" style="background-image: url('./data/images/275x385.jpg'); width: 100%;height: 100%">
-                                <div class="hoverInfo showHover showingDetail" data-id="2237" data-slug="2237-biet-doi-bat-hao">
-                                    <div class="icon-play"><span><i class="f-movie-icon-play"></i></span></div>
+
+                    <c:each from="{$phim_hot.top}" key="$key" value="$film">
+
+                        <div class="f_item">
+                            <a href="{$film.link}" title="{$film.title}">
+                                <div class="f_filmthumb" style="background-image: url('./data/images/275x385.jpg'); width: 100%;height: 100%">
+                                    <div class="hoverInfo showHover showingDetail" data-id="2237" data-slug="2237-biet-doi-bat-hao">
+                                        <div class="icon-play"><span><i class="f-movie-icon-play"></i></span></div>
+                                    </div>
+                                    <span class="ng-binding ageType">C18</span>
+                                    <span class="avgPoint ng-scope"><p class="txtPoint ng-binding">110 phút</p></span>
                                 </div>
-                                <span class="ng-binding ageType">C18</span>
-                                <span class="avgPoint ng-scope"><p class="txtPoint ng-binding">110 phút</p></span>
+                            </a>
+                            <div class="f_item_option">
+                                <i class="fas fa-volume-down" title="Thuyết minh"></i>
+                                <i class="fas fa-comment-dots" title="VietSub"></i>
                             </div>
-                        </a>
-                        <div class="f_item_option">
-                            <i class="fas fa-volume-down" title="Thuyết minh"></i>
-                            <i class="fas fa-comment-dots" title="VietSub"></i>
-                        </div>
-                        <div class="info">
-                            <div class="ng-binding ng-scope">
-                                <a href="http://google.com" title="Để mai tính" title="Biệt Đội Bất Hảo"><span class="f_title_vn">Biệt Đội Bất Hảo</span> <span class="f_title_en">The Bad Guys: Reign of Chaos (C18)</span></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="f_item">
-                        <a href="http://google.com" title="Để mai tính">
-                            <div class="f_filmthumb" style="background-image: url('./data/images/275x385.jpg')">
-                                <div class="hoverInfo showHover showingDetail" data-id="2237" data-slug="2237-biet-doi-bat-hao">
-                                    <div class="icon-play"><span><i class="f-movie-icon-play"></i></span></div>
+                            <div class="info">
+                                <div class="ng-binding ng-scope">
+                                    <a href="{$film.link}" title="{$film.title}"><span class="f_title_vn">{$film.title}</span> <span class="f_title_en">{$film.title_o}</span></a>
                                 </div>
-                                <span class="ng-binding ageType">C18</span>
-                                <span class="avgPoint ng-scope"><p class="txtPoint ng-binding">120 phút</p></span>
-                            </div>
-                        </a>
-                        <div class="info">
-                            <div class="ng-binding ng-scope">
-                                <a href="http://google.com" title="Để mai tính" title="Biệt Đội Bất Hảo"><span class="f_title_vn">Biệt Đội Bất Hảo</span><span class="f_title_en">The Bad Guys: Reign of Chaos (C18)</span></a>
                             </div>
                         </div>
-                    </div>
-                    <div class="f_item">
-                        <a href="http://google.com" title="Để mai tính">
-                            <div class="f_filmthumb" style="background-image: url('./data/images/275x385.jpg')">
-                                <div class="hoverInfo showHover showingDetail" data-id="2237" data-slug="2237-biet-doi-bat-hao">
-                                    <div class="icon-play"><span><i class="f-movie-icon-play"></i></span></div>
-                                </div>
-                                <span class="ng-binding ageType">HD</span>
-                                <span class="avgPoint ng-scope"><p class="txtPoint ng-binding">30 phút</p></span>
-                            </div>
-                        </a>
-                        <div class="info">
-                            <div class="ng-binding ng-scope">
-                                <a href="http://google.com" title="Để mai tính" title="Biệt Đội Bất Hảo"><span class="f_title_vn">Biệt Đội Bất Hảo</span><span class="f_title_en">The Bad Guys: Reign of Chaos (C18)</span></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="f_item">
-                        <a href="http://google.com" title="Để mai tính">
-                            <div class="f_filmthumb" style="background-image: url('./data/images/275x385.jpg')">
-                                <div class="hoverInfo showHover showingDetail" data-id="2237" data-slug="2237-biet-doi-bat-hao">
-                                    <div class="icon-play"><span><i class="f-movie-icon-play"></i></span></div>
-                                </div>
-                                <span class="ng-binding ageType">FULL</span>
-                                <span class="avgPoint ng-scope"><p class="txtPoint ng-binding">145 phút</p></span>
-                            </div>
-                        </a>
-                        <div class="info">
-                            <div class="ng-binding ng-scope">
-                                <a href="http://google.com" title="Để mai tính" title="Biệt Đội Bất Hảo"><span class="f_title_vn">Biệt Đội Bất Hảo</span><span class="f_title_en">The Bad Guys: Reign of Chaos (C18)</span></a>
-                            </div>
-                        </div>
-                    </div>
+
+                    </c:each>
                 </div>
                 <div class="carousel-item">
-                    <div class="f_item">
-                        <a href="http://google.com" title="Để mai tính">
-                            <div class="f_filmthumb" style="background-image: url('./data/images/275x385.jpg')">
-                                <div class="hoverInfo showHover showingDetail" data-id="2237" data-slug="2237-biet-doi-bat-hao">
-                                    <div class="icon-play"><span><i class="f-movie-icon-play"></i></span></div>
+                <c:each from="{$phim_hot.last}" key="$key" value="$film">
+
+
+                        <div class="f_item">
+                            <a href="{$film.link}" title="{$film.title}">
+                                <div class="f_filmthumb" style="background-image: url('./data/images/275x385.jpg')">
+                                    <div class="hoverInfo showHover showingDetail" data-id="2237" data-slug="2237-biet-doi-bat-hao">
+                                        <div class="icon-play"><span><i class="f-movie-icon-play"></i></span></div>
+                                    </div>
+                                    <span class="ng-binding ageType">VietSub</span>
+                                    <span class="avgPoint ng-scope"><p class="txtPoint ng-binding">90 phút</p></span>
                                 </div>
-                                <span class="ng-binding ageType">VietSub</span>
-                                <span class="avgPoint ng-scope"><p class="txtPoint ng-binding">90 phút</p></span>
-                            </div>
-                        </a>
-                        <div class="info">
-                            <div class="ng-binding ng-scope">
-                                <a href="http://google.com" title="Để mai tính" title="Biệt Đội Bất Hảo"><span class="f_title_vn">Biệt Đội Bất Hảo</span><span class="f_title_en">The Bad Guys: Reign of Chaos (C18)</span></a>
+                            </a>
+                            <div class="info">
+                                <div class="ng-binding ng-scope">
+                                    <a href="{$film.link}" title="{$film.title}"><span class="f_title_vn">{$film.title}</span><span class="f_title_en">{$film.title_o}</span></a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="f_item">
-                        <a href="http://google.com" title="Để mai tính">
-                            <div class="f_filmthumb" style="background-image: url('./data/images/275x385.jpg')">
-                                <div class="hoverInfo showHover showingDetail" data-id="2237" data-slug="2237-biet-doi-bat-hao">
-                                    <div class="icon-play"><span><i class="f-movie-icon-play"></i></span></div>
-                                </div>
-                                <span class="ng-binding ageType">HD</span>
-                                <span class="avgPoint ng-scope"><p class="txtPoint ng-binding">120 phút</p></span>
-                            </div>
-                        </a>
-                        <div class="info">
-                            <div class="ng-binding ng-scope">
-                                <a href="http://google.com" title="Để mai tính" title="Biệt Đội Bất Hảo"><span class="f_title_vn">Biệt Đội Bất Hảo</span><span class="f_title_en">The Bad Guys: Reign of Chaos (C18)</span></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="f_item">
-                        <a href="http://google.com" title="Để mai tính">
-                            <div class="f_filmthumb" style="background-image: url('./data/images/275x385.jpg')">
-                                <div class="hoverInfo showHover showingDetail" data-id="2237" data-slug="2237-biet-doi-bat-hao">
-                                    <div class="icon-play"><span><i class="f-movie-icon-play"></i></span></div>
-                                </div>
-                                <span class="ng-binding ageType">FULL HD</span>
-                                <span class="avgPoint ng-scope"><p class="txtPoint ng-binding">130 phút</p></span>
-                            </div>
-                        </a>
-                        <div class="info">
-                            <div class="ng-binding ng-scope">
-                                <a href="http://google.com" title="Để mai tính" title="Biệt Đội Bất Hảo"><span class="f_title_vn">Biệt Đội Bất Hảo</span><span class="f_title_en">The Bad Guys: Reign of Chaos (C18)</span></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="f_item">
-                        <a href="http://google.com" title="Để mai tính">
-                            <div class="f_filmthumb" style="background-image: url('./data/images/275x385.jpg')">
-                                <div class="hoverInfo showHover showingDetail" data-id="2237" data-slug="2237-biet-doi-bat-hao">
-                                    <div class="icon-play"><span><i class="f-movie-icon-play"></i></span></div>
-                                </div>
-                                <span class="ng-binding ageType">HD</span>
-                                <span class="avgPoint ng-scope"><p class="txtPoint ng-binding">110 phút</p></span>
-                            </div>
-                        </a>
-                        <div class="info">
-                            <div class="ng-binding ng-scope">
-                                <a href="http://google.com" title="Để mai tính" title="Biệt Đội Bất Hảo"><span class="f_title_vn">Biệt Đội Bất Hảo</span><span class="f_title_en">The Bad Guys: Reign of Chaos (C18)</span></a>
-                            </div>
-                        </div>
-                    </div>
+
+                </c:each>
                 </div>
-                <div class="carousel-item">
-                    <div class="f_item">
-                        <a href="http://google.com" title="Để mai tính">
-                            <div class="f_filmthumb" style="background-image: url('./data/images/275x385.jpg')">
-                                <div class="hoverInfo showHover showingDetail" data-id="2237" data-slug="2237-biet-doi-bat-hao">
-                                    <div class="icon-play"><span><i class="f-movie-icon-play"></i></span></div>
-                                </div>
-                                <span class="ng-binding ageType">VietSub</span>
-                                <span class="avgPoint ng-scope"><p class="txtPoint ng-binding">70 phút</p></span>
-                            </div>
-                        </a>
-                        <div class="info">
-                            <div class="ng-binding ng-scope">
-                                <a href="http://google.com" title="Để mai tính" title="Biệt Đội Bất Hảo"><span class="f_title_vn">Biệt Đội Bất Hảo</span><span class="f_title_en">The Bad Guys: Reign of Chaos (C18)</span></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="f_item">
-                        <a href="http://google.com" title="Để mai tính">
-                            <div class="f_filmthumb" style="background-image: url('./data/images/275x385.jpg')">
-                                <div class="hoverInfo showHover showingDetail" data-id="2237" data-slug="2237-biet-doi-bat-hao">
-                                    <div class="icon-play"><span><i class="f-movie-icon-play"></i></span></div>
-                                </div>
-                                <span class="ng-binding ageType">HD</span>
-                                <span class="avgPoint ng-scope"><p class="txtPoint ng-binding">130 phút</p></span>
-                            </div>
-                        </a>
-                        <div class="info">
-                            <div class="ng-binding ng-scope">
-                                <a href="http://google.com" title="Để mai tính" title="Biệt Đội Bất Hảo"><span class="f_title_vn">Biệt Đội Bất Hảo</span><span class="f_title_en">The Bad Guys: Reign of Chaos (C18)</span></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="f_item">
-                        <a href="http://google.com" title="Để mai tính">
-                            <div class="f_filmthumb" style="background-image: url('./data/images/275x385.jpg')">
-                                <div class="hoverInfo showHover showingDetail" data-id="2237" data-slug="2237-biet-doi-bat-hao">
-                                    <div class="icon-play"><span><i class="f-movie-icon-play"></i></span></div>
-                                </div>
-                                <span class="ng-binding ageType">C18</span>
-                                <span class="avgPoint ng-scope"><p class="txtPoint ng-binding">120 phút</p></span>
-                            </div>
-                        </a>
-                        <div class="info">
-                            <div class="ng-binding ng-scope">
-                                <a href="http://google.com" title="Để mai tính" title="Biệt Đội Bất Hảo"><span class="f_title_vn">Biệt Đội Bất Hảo</span><span class="f_title_en">The Bad Guys: Reign of Chaos (C18)</span></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="f_item">
-                        <a href="http://google.com" title="Để mai tính">
-                            <div class="f_filmthumb" style="background-image: url('./data/images/275x385.jpg')">
-                                <div class="hoverInfo showHover showingDetail" data-id="2237" data-slug="2237-biet-doi-bat-hao">
-                                    <div class="icon-play"><span><i class="f-movie-icon-play"></i></span></div>
-                                </div>
-                                <span class="ng-binding ageType">C18</span>
-                                <span class="avgPoint ng-scope"><p class="txtPoint ng-binding">90 phút</p></span>
-                            </div>
-                        </a>
-                        <div class="info">
-                            <div class="ng-binding ng-scope">
-                                <a href="http://google.com" title="Để mai tính" title="Biệt Đội Bất Hảo"><span class="f_title_vn">Biệt Đội Bất Hảo</span><span class="f_title_en">The Bad Guys: Reign of Chaos (C18)</span></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+
+
             </div>
 
             <!-- Left and right controls -->
